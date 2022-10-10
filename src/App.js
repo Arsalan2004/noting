9073eb94f4411c820af7b35a42458bdeb20ba393
Arsalan2004 <taufikshaikh733@gmail.com>
2022-10-10 16:21:16 +0530

    localstorage added

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import React,{useState} from 'react'
+import React, { useState, useEffect } from 'react'
 import Header from './Components/Header';
 import Createnote from './Components/Createnote';
 // import Shownote from './Components/Shownote';
@@ -6,38 +6,52 @@ import Shownotes from './Components/Shownotes';
 // import Bottom from './Components/Bottom';
 
 export default function App() {
-  const deleteList=(id)=>{
-    console.log('deleted'+id);
-    setitems((olditems)=>{
-     return olditems.filter((arr,index)=>{
-        return index!==id
+  const deleteList = (id) => {
+    console.log('deleted' + id);
+    setitems((olditems) => {
+      return olditems.filter((arr, index) => {
+        return index !== id
       })
     })
   }
-  const[items,setitems]=useState([])
-  const additem=(value)=>{
-   setitems((olditems)=>{
-    return [...olditems,value]
-   });
+
+  const getitemfromlocal = () => {
+    const notes = localStorage.getItem("mynote");
+
+    if (notes) {
+      return JSON.parse(notes);
+    }
+    else {
+      return [];
+    }
   }
+  const [items, setitems] = useState(getitemfromlocal)
+  const additem = (value) => {
+    setitems((olditems) => {
+      return [...olditems, value]
+    });
+  }
+  useEffect(() => {
+    localStorage.setItem("mynote", JSON.stringify(items));
+  }, [items])
   return (
     <>
-    <Header/>
-    <Createnote onclick={additem}/>
+      <Header />
+      <Createnote onclick={additem} />
 
-<div className='shownote'>
-    {
-      items.map((val,index)=>{
-    return(<Shownotes  
-          title={val.title} 
-          content={val.content}
-          id={index}
-          key={index}
-          ondelete={deleteList}
-          />)
-      })
-    }
-    </div>
+      <div className='shownote'>
+        {
+          items.map((val, index) => {
+            return (<Shownotes
+              title={val.title}
+              content={val.content}
+              id={index}
+              key={index}
+              ondelete={deleteList}
+            />)
+          })
+        }
+      </div>
     </>
   )
 }
